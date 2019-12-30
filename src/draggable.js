bd11ddeff9b64a20c4ae47e68bc0e9caa1575639
Joshua Lochner <admin@xenova.com>
2019-12-30 05:03:00 +0200

    Draggable

--- src/draggable.js
+++ src/draggable.js
@@ -11,33 +11,86 @@ Element.prototype.DraggableJS = function (o) {
     const defaultOptions = {
         containment: 'none', //parent, none
         axis: 'xy', //x, y, xy
+        momentum: true,
+        refreshTime: 25,
+        maxEaseTime: 1,
     }
     self.options = Object.assign({}, defaultOptions, o);
 
+    self.stationary = null;
+    // self.easing = null;
+    self.mouseTracking = [];
+    self.current = [];
     console.log(self.options);
-    this.mouseDown = false;
+    self.mouseDown = false;
 
     // console.log(this.parentNode);
-    this.style.position = 'absolute';
+    self.style.position = 'absolute';
 
 
 
-    this.addEventListener('mousedown', function (e) {
+    self.addEventListener('mousedown', function (e) {
         var matrix = new WebKitCSSMatrix(self.style.webkitTransform);
         // console.log(matrix);
 
         self.mouseDown = true;
         self.startX = e.pageX - matrix.m41;
         self.startY = e.pageY - matrix.m42;
+
+        self.mouseTracking = [[e.pageX, e.pageY], [e.pageX, e.pageY]];
+        console.log(self.mouseTracking);
     });
     document.addEventListener('mouseup', function (e) {
+        if (self.mouseDown) {
+            let deltaX = (self.mouseTracking[1][0] - self.mouseTracking[0][0]);
+            let deltaY = (self.mouseTracking[1][1] - self.mouseTracking[0][1]);
+
+
+            let speed = 1000 * Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)) / self.options.refreshTime;
+
+            // console.log(speed + ' pixels per second');
+            // console.log(Math.atan2(-deltaY, deltaX));
+
+            var counter = 0;
+
+            var duration = ((-1) / ((speed / 1920) + 1) + 1)*1000;
+            console.log("DURATION: " + duration)
+            var x = setInterval(function () {
+                if ((counter += self.options.refreshTime) <= duration) {
+                    console.log(counter);
+                } else {
+                    clearInterval(x);
+                }
+            }, self.options.refreshTime);
+
+            self.mouseTracking = [];
+        }
         self.mouseDown = false;
     });
 
+    setInterval(function () {
+        if (self.mouseDown) {
+            self.mouseTracking.push(self.current);
+            self.mouseTracking.shift();
+        }
+    }, self.options.refreshTime);
+    //per 25ms
     document.addEventListener('mousemove', function (e) {
         self.mouseDown &= (e.buttons === undefined ? e.which === 1 : e.buttons === 1);
-        // console.log(e.pageX,self.startX,(e.pageX-self.startX));
+
+
         if (self.mouseDown) {
+            self.current = [e.pageX, e.pageY];
+            // self.mouseTracking.push([e.pageX, e.pageY]);
+            // self.mouseTracking.shift();
+
+            // clearTimeout(self.stationary);
+            // self.stationary = setTimeout(function () {
+            //     self.mouseTracking.push(self.mouseTracking[1]);
+            //     self.mouseTracking.shift();
+            // }, 100);
+
+
             let parentInfo = self.parentNode.getBoundingClientRect();
             let childInfo = self.getBoundingClientRect();
 
@@ -55,13 +108,14 @@ Element.prototype.DraggableJS = function (o) {
                     break;
             }
 
-            x =  (self.options.axis.toLowerCase().indexOf('x')>-1) ? (x+'px'):0;
-            y =  (self.options.axis.toLowerCase().indexOf('y')>-1) ? (y+'px'):0;
-            
+            x = (self.options.axis.toLowerCase().indexOf('x') > -1) ? (x + 'px') : 0;
+            y = (self.options.axis.toLowerCase().indexOf('y') > -1) ? (y + 'px') : 0;
+
             self.style.transform = 'translate3d(' + x + ',' + y + ',0)';
             // console.log('translate3d(' + x + ',' + y + ',0)');
-            
+            // console.log(e.pageX, e.pageY);
         }
+
     });
     // console.log(this);
     // this.innerHTML = 'aaa';
